53816b2439b06249c3e5a3fe51ad804533eecc96
EshaBidkar <eshabidkar@gmail.com>
2025-09-24 19:07:11 +0530

    Home page added

---
+++ Frontend_Home/internship-homepage/src/components/layout/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar } from '../common';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <main>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
